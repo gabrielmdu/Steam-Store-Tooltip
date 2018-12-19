8c5e5af9058504c4ecfd23a3ef7c9dae25c1b264
Gabriel Schulte <gabrielmdu@gmail.com>
2018-12-19 11:26:01 -0200

    Moved files & added new gulp functions.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,41 @@
 const { parallel, src, dest } = require('gulp');
+const all = require('gulp-all')
 const del = require('del');
 const cleancss = require('gulp-clean-css');
 const htmlmin = require('gulp-htmlmin');
 const uglify = require('gulp-uglify-es').default;
+const jsonminify = require('gulp-jsonminify');
 
 function css() {
-    return src('css/*.css')
+    return src('src/css/*.css')
         .pipe(cleancss())
         .pipe(dest('dist/css/'));
 }
 
 function html() {
-    return src('html/*.html')
+    return src('src/html/*.html')
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(dest('dist/html/'));
 }
 
 function js() {
-    return src('js/*.js')
+    return src('src/js/*.js')
         .pipe(uglify())
+        .pipe(dest('dist/js/'));
+}
+
+function rsc() {
+    return all([
+        src('src/manifest.json')
+            .pipe(jsonminify())
+            .pipe(dest('dist')),
+        src('src/img/*.png')
+            .pipe(dest('dist/img'))
+    ])
+}
+
+function vendor() {
+    return src('node_modules/tippy.js/dist/tippy.all.min.js')
         .pipe(dest('dist/js/'));
 }
 
@@ -26,5 +43,6 @@ function clean() {
     return del('dist/**')
 }
 
-exports.build = parallel(css, html, js);
+exports.build = parallel(css, html, js, rsc);
+exports.vendor = vendor;
 exports.clean = clean;
