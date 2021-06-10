6851b011e4332d0c1eb3d4ecf9ab142e965e6ecf
Gabriel Schulte <gabrielmdu@gmail.com>
2021-06-10 16:19:48 +0300

    Moved icon conversion and resizing to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const JsonMinimizerPlugin = require('json-minimizer-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin');
 
 module.exports = {
     //mode: 'production',
@@ -10,6 +11,7 @@ module.exports = {
         background: './src/js/background.js',
         options: './src/js/options.js',
         steamstoretooltip: './src/js/steamstoretooltip.js',
+        icon: './src/js/icon.js'
     },
     output: {
         filename: 'js/[name].js',
@@ -32,7 +34,7 @@ module.exports = {
                             [
                                 '@babel/preset-env',
                                 {
-                                    modules: "umd",
+                                    modules: 'umd',
                                     targets: {
                                         chrome: 91,
                                     }
@@ -45,10 +47,26 @@ module.exports = {
             {
                 test: /\.s?css$/,
                 use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader",
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
                 ]
+            },
+            {
+                test: /icon\.svg/,
+                use: {
+                    loader: 'svg-to-png-loader',
+                    options: {
+                        name: 'icon[width].png',
+                        sizes: [
+                            '128',
+                            '48',
+                            '32',
+                            '16'
+                        ],
+                        outputPath: './img'
+                    }
+                }
             }
         ]
     },
@@ -60,12 +78,19 @@ module.exports = {
                     from: '*.json',
                 },
                 {
-                    context: path.resolve(__dirname, "./src/html"),
-                    from: "*.html",
+                    context: path.resolve(__dirname, './src/html'),
+                    from: '*.html',
                     to: './html'
-                }
+                },
             ],
         }),
+        new RemovePlugin({
+            after: {
+                include: [
+                    './dist/js/icon.js'
+                ]
+            }
+        })
     ],
     optimization: {
         minimizer: [
